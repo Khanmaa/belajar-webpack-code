b6c1bf8168bb9ece5d6b27fc2cf6ba8b834caf20
Khanmaa <imludociel10@gmail.com>
2023-09-07 03:44:02 +0800

    Menambahkan ES6 Module

--- src/app/alert.service.js
+++ src/app/alert.service.js
@@ -1,4 +1,7 @@
-class AlertService {
+
+import { cekInputValid } from "./utils/cekInputValid.js";
+
+export  class AlertService {
   constructor() {
     this.error = document.querySelector('#error');
   }
